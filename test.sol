53a21db14b766acbd1775d5f41f4702b4dc59274
Iwon <ygy2529@gmail.com>
2021-03-18 14:22:06 +0900

    Add files via upload

--- test.sol
+++ test.sol
@@ -7,6 +7,15 @@ contract Example005 {
     result = multi(11,5);
     result = devide(10,5);
   }
+  function sub_op() returns (uint result) {
+    result = subtract(11,5);
+  }
+  function multi_op() returns (uint result) {
+    result = multi(11,5);
+  }
+  function devide_op() returns (uint result) {
+    result = devide(10,5);
+  }
 
   function sum(uint a, uint b) returns (uint) {
       uint result = a + b;
